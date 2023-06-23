1bd76bd3dc916f4050eb76dc53ae678ad728c2bd
ganbari <steinway.nathan@gmail.com>
2023-06-23 14:00:20 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`I hate ${port} mixups`))
